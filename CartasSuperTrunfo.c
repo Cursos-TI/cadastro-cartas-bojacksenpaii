b4c93444db57c62505806d4530e6ac4d152a1e4c
bojacksenpaii <bojacksenpaii@gmail.com>
2026-05-12 21:04:54 +0000

    Adicionei a biblioteca "string.h" para o uso de "strcspn" a fim de evitar que ao uso do fgets tenha algum risco de "\n" ser adicionado ao buffer no texto do usuário; adicionei também o próprio "fgets" ao invés de scanf para leitura de strings vindas do teclado do usuário, por ser uma função mais segura e menos passível de erros. Adicionei também a função "getchar()" no momento em que começo a codigicar a segunda carta e os pedidos de entrada para o usuário; o fiz por ser uma solução para esvaziar o buffer e evitar um erro constante que estava ocorrendo sobre "\n" gerado pelo "Enter". De restante, o código aplica tudo o que o professor ensinou em aula (funções como: scanf, print f, as variáveis, os ponteiros, conjuntos...).

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,15 +1,101 @@
 #include <stdio.h>
+#include <string.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das cartas
-// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
+int main() { 
+    //Carta 1
+    char letra[3];
+    char codigo[5];
+    char cidade[50];
+    
+    int populacao, turismo;
+    float area, pib;
+    
+    //Carta 2
+    char estado[3];
+    char carta[5];
+    char nome[50];
+    
+    int pessoas, pontos;
+    float tamanho, valor;
 
-int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+    //Codificando carta 1
+    printf("Informe os dados para a primeira carta:\n");
+    printf("Insira uma letra de 'A' a 'H' para representar o Estado da sua carta: \n");
+    fgets(letra, 3, stdin);
+    letra[strcspn(letra, "\n")] = '\0';
 
-  // Área para entrada de dados
+    printf("Insira um numero entre 01 a 04 para codificar a sua carta: \n");
+    fgets(codigo, 5, stdin);
+    codigo[strcspn(codigo, "\n")] = '\0';
 
-  // Área para exibição dos dados da cidade
+    printf("Insira o nome da cidade: \n");
+    fgets(cidade, 50, stdin);
+    cidade[strcspn(cidade, "\n")] = '\0';
 
-return 0;
-} 
+    printf("Insira o tamanho populacional da cidade: \n");
+    scanf("%d", &populacao);
+
+    printf("Insira o tamanho em Km² da cidade: \n");
+    scanf("%f", &area);
+
+    printf("Insira o PIB da cidade: \n");
+    scanf("%f", &pib);
+    
+    printf("Insira o numero de pontos turisticos existentes na cidade: \n");
+    scanf("%d", &turismo);
+
+
+    //Codificando carta 2
+    printf("\nInforme os dados para a segunda carta:\n");
+
+    getchar();
+
+    printf("Insira uma letra de 'A' a 'H' para representar o Estado da sua carta: \n");
+    fgets(estado, 3, stdin);
+    estado[strcspn(estado, "\n")] = '\0';
+
+    printf("Insira um numero entre 01 a 04 para codificar a sua carta: \n");
+    fgets(carta, 5, stdin);
+    carta[strcspn(carta, "\n")] = '\0';
+
+    printf("Insira o nome da cidade: \n");
+    fgets(nome, 50, stdin);
+    nome[strcspn(nome, "\n")] = '\0';
+
+    printf("Insira o tamanho populacional da cidade: \n");
+    scanf("%d", &pessoas);
+
+    printf("Insira o tamanho em Km² da cidade: \n");
+    scanf("%f", &tamanho);
+
+    printf("Insira o PIB da cidade: \n");
+    scanf("%f", &valor);
+    
+    printf("Insira o numero de pontos turisticos existentes na cidade: \n");
+    scanf("%d", &pontos);
+
+    //Exibição da carta 1
+
+    printf("\nCarta 1:\n");
+    printf("Estado: %s\n", letra);
+    printf("Codigo: %s%s\n", letra, codigo);
+    printf("Nome da Cidade: %s\n", cidade);
+    printf("Populacao: %d\n", populacao);
+    printf("Area (em Km²): %.2f\n", area);
+    printf("PIB: %.2f\n", pib);
+    printf("Pontos Turisticos: %d\n", turismo);
+
+    //Exibição da carta 2
+
+    printf("\nCarta 2:\n");
+    printf("Estado: %s\n", estado);
+    printf("Codigo: %s%s\n", estado, carta);
+    printf("Nome da Cidade: %s\n", nome);
+    printf("Populacao: %d\n", pessoas);
+    printf("Area (em Km²): %.2f\n", tamanho);
+    printf("PIB: %.2f\n", valor);
+    printf("Pontos Turisticos: %d\n", pontos);
+
+    return 0;
+
+}
